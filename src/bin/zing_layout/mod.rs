3dff6514509d16cc3e3e1a1df5ca3b459f984728
Hans Meine <hans_meine@gmx.net>
2022-10-25 21:16:51 +0200

    test, finish and finetune card stacks layout
    - move camera setup into separate system
    - add (temporary) card rendering code for testing
    - fix y coordinate system direction
    - implement zoomed own hand
    - slightly reduce CARD_HEIGHT

--- src/bin/zing_layout/mod.rs
+++ src/bin/zing_layout/mod.rs
@@ -1,10 +1,12 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_svg::prelude::*;
 use zing_rs::card_action::CardLocation;
 
 pub struct LayoutPlugin;
 
 impl Plugin for LayoutPlugin {
     fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_camera);
         app.add_startup_system(setup_card_stacks);
     }
 }
@@ -12,7 +14,7 @@ impl Plugin for LayoutPlugin {
 /// margin at top / bottom of screen (relative to full screen height)
 const MARGIN: f32 = 0.05;
 /// vertical base size of all cards (relative to full screen height)
-const CARD_HEIGHT: f32 = 0.25;
+const CARD_HEIGHT: f32 = 0.23;
 /// horizontal base size of all cards
 const CARD_WIDTH: f32 = CARD_HEIGHT / 1.4;
 /// offset for spreading out cards on player hands
@@ -29,7 +31,7 @@ const VERTICAL_SPACING: f32 = (1. - 2. * MARGIN - (2. + OWN_CARD_ZOOM) * CARD_HE
 /// reserving some space at the right for the score stacks, the center should shift to left
 const PLAYING_CENTER_X: f32 = -0.6 * CARD_WIDTH;
 /// reserving some space at the bottom for the zoomed in own hand, the center should be above 0
-const PLAYING_CENTER_Y: f32 = (1. - OWN_CARD_ZOOM) * CARD_HEIGHT / 2.;
+const PLAYING_CENTER_Y: f32 = (OWN_CARD_ZOOM - 1.) * CARD_HEIGHT / 2.;
 
 // TODO: we need to consider having four players
 
@@ -45,6 +47,7 @@ struct CardStack {
 fn spawn_card_stack(
     commands: &mut Commands,
     top_left_position: Vec3,
+    scale: Vec3,
     location: CardLocation,
     index: usize,
 ) -> Entity {
@@ -52,6 +55,7 @@ fn spawn_card_stack(
         .spawn_bundle(SpatialBundle {
             transform: Transform {
                 translation: top_left_position,
+                scale,
                 ..Default::default()
             },
             ..Default::default()
@@ -60,9 +64,7 @@ fn spawn_card_stack(
         .id()
 }
 
-pub fn setup_card_stacks(mut commands: Commands) {
-    let opposite_hand_pos_y = PLAYING_CENTER_Y - VERTICAL_SPACING - 1.5 * CARD_HEIGHT;
-
+pub fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle {
         projection: OrthographicProjection {
             scaling_mode: ScalingMode::FixedVertical(1.0),
@@ -70,74 +72,107 @@ pub fn setup_card_stacks(mut commands: Commands) {
         },
         ..Default::default()
     });
+}
 
-    spawn_card_stack(
+pub fn setup_card_stacks(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let opposite_hand_pos_y = PLAYING_CENTER_Y + VERTICAL_SPACING + 1.5 * CARD_HEIGHT;
+
+    let mut stacks = Vec::new();
+
+    stacks.push(spawn_card_stack(
         &mut commands,
         Vec3::new(
             PLAYING_CENTER_X - FULL_HAND_WIDTH / 2.,
             opposite_hand_pos_y,
             0.,
         ),
+        Vec3::ONE,
         CardLocation::PlayerHand,
         0, // FIXME: we need to know which player we are
-    );
+    ));
 
-    let own_hand_pos_y = PLAYING_CENTER_Y + 0.5 * CARD_HEIGHT + VERTICAL_SPACING;
+    let own_hand_pos_y = PLAYING_CENTER_Y - 0.5 * CARD_HEIGHT - VERTICAL_SPACING;
 
-    spawn_card_stack(
+    stacks.push(spawn_card_stack(
         &mut commands,
         Vec3::new(
             PLAYING_CENTER_X - FULL_HAND_WIDTH * OWN_CARD_ZOOM / 2.,
             own_hand_pos_y,
             0.,
         ),
+        Vec3::splat(OWN_CARD_ZOOM),
         CardLocation::PlayerHand,
         1, // FIXME: we need to know which player we are
-    );
+    ));
 
     // TODO: we need to know if we have two or four players
 
-    spawn_card_stack(
+    stacks.push(spawn_card_stack(
         &mut commands,
         Vec3::new(
             PLAYING_CENTER_X - CARD_WIDTH * 2.3,
-            PLAYING_CENTER_Y - CARD_HEIGHT / 2.,
+            PLAYING_CENTER_Y + CARD_HEIGHT / 2.,
             0.,
         ),
+        Vec3::ONE,
         CardLocation::Stack,
         0, // "stock"
-    );
+    ));
 
-    spawn_card_stack(
+    stacks.push(spawn_card_stack(
         &mut commands,
         Vec3::new(
             PLAYING_CENTER_X - CARD_WIDTH / 2.,
-            PLAYING_CENTER_Y - CARD_HEIGHT / 2.,
+            PLAYING_CENTER_Y + CARD_HEIGHT / 2.,
             0.,
         ),
+        Vec3::ONE,
         CardLocation::Stack,
         1, // "table"
-    );
+    ));
 
-    spawn_card_stack(
+    stacks.push(spawn_card_stack(
         &mut commands,
         Vec3::new(
             PLAYING_CENTER_X + FULL_HAND_WIDTH * OWN_CARD_ZOOM / 2. + SCORE_STACK_SPACING,
             opposite_hand_pos_y,
             0.,
         ),
+        Vec3::ONE,
         CardLocation::Stack,
         2, // "score_0"
-    );
+    ));
 
-    spawn_card_stack(
+    stacks.push(spawn_card_stack(
         &mut commands,
         Vec3::new(
             PLAYING_CENTER_X + FULL_HAND_WIDTH * OWN_CARD_ZOOM / 2. + SCORE_STACK_SPACING,
             own_hand_pos_y,
             0.,
         ),
+        Vec3::ONE,
         CardLocation::Stack,
         3, // "score_1"
-    );
+    ));
+
+    for stack in stacks {
+        let svg = asset_server.load("vector_cards_3.2/CLUB-6.svg");
+        // SVG size: 238.11075, 332.5986
+        let scale = CARD_HEIGHT / 332.5986;
+
+        let card = commands
+            .spawn_bundle(Svg2dBundle {
+                svg,
+                transform: Transform {
+                    scale: Vec3::new(scale, scale, 1.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Card)
+            .id();
+        commands.entity(stack).push_children(&[card]);
+    }
+
+    //    let svg = asset_server.load("vector_cards_3.2/BACK-BLUE.svg"));
 }
