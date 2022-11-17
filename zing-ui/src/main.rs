c823557a34e32d4c4471e87031e029f54eb0a8a9
Hans Meine <hans_meine@gmx.net>
2022-11-17 21:16:16 +0100

    adapt zing-ui to removed Table

--- zing-ui/src/main.rs
+++ zing-ui/src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_tweening::TweeningPlugin;
 use game_state::GameState;
-use zing_game::{table::Table, zing_game::ZingGame};
+use zing_game::zing_game::ZingGame;
 
 mod card_sprite;
 mod constants;
@@ -9,17 +9,6 @@ mod game_state;
 mod zing_layout;
 
 fn main() {
-    let table = Table {
-        players: vec![
-            zing_game::table::Player {
-                name: "Hans".into(),
-            },
-            zing_game::table::Player {
-                name: "Darko".into(),
-            },
-        ],
-    };
-
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .insert_resource(ClearColor(Color::rgb_u8(0x33, 0x69, 0x1d)))
@@ -30,7 +19,10 @@ fn main() {
             fit_canvas_to_parent: true,
             ..Default::default()
         })
-        .insert_resource(GameState::new(ZingGame::new_from_table(table, 0), 0))
+        .insert_resource(GameState::new(
+            ZingGame::new_with_player_names(vec!["Hans".into(), "Darko".into()], 1),
+            0,
+        ))
         .add_plugins(DefaultPlugins)
         .add_plugin(TweeningPlugin)
         //.insert_resource(game.state())
