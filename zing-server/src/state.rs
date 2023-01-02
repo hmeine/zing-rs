hmeine/zing-rs

commit e8eb817551f1465499d922ebb1d9b649e126cc51

let axum add application/json content type

zing-server/src/state.rs
@@ -1,8 +1,4 @@
-use axum::{
-    http::{self, header},
-    response::IntoResponse,
-    Json,
-};
+use axum::{http, response::IntoResponse, Json};
 use chrono::prelude::*;
 use rand::distributions::{Alphanumeric, DistString};
 use serde::{Serialize, Serializer};
@@ -181,8 +177,7 @@ impl ZingState {
     pub fn list_tables(&self, login_id: &str) -> Result<impl IntoResponse, ErrorResponse> {
         let user = self.get_user(login_id)?;
 
-        Ok((
-            [(header::CONTENT_TYPE, "application/json")],
+        Ok(Json(
             serde_json::to_string(
                 &user
                     .tables

commit 7112c4011bd35870b3b27610dd034ea38f256afa

revert e8eb817 which broke JSON response
Json(serde_json::to_string(...)) returns an encoded *string*, not the
JSON object anymore.  Without serde_json::to_string(), however, we have
a complex datastructure with references, which causes lifetime problems.
We don't want to duplicate the whole datastructure, so reverting seems
to be the best trade-off between clean code and complexity elsewhere.

zing-server/src/state.rs
@@ -1,4 +1,8 @@
-use axum::{http, response::IntoResponse, Json};
+use axum::{
+    http::{self, header},
+    response::IntoResponse,
+    Json,
+};
 use chrono::prelude::*;
 use rand::distributions::{Alphanumeric, DistString};
 use serde::{Serialize, Serializer};
@@ -187,7 +191,8 @@ impl ZingState {
     pub fn list_tables(&self, login_id: &str) -> Result<impl IntoResponse, ErrorResponse> {
         let user = self.get_user(login_id)?;
 
-        Ok(Json(
+        Ok((
+            [(header::CONTENT_TYPE, "application/json")],
             serde_json::to_string(
                 &user
                     .tables