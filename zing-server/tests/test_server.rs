b230480605bb3cf4ce6f62a00ba3ed8f08be983a
Hans Meine <hans_meine@gmx.net>
2023-04-24 21:37:49 +0200

    also test creating tables

--- zing-server/tests/test_server.rs
+++ zing-server/tests/test_server.rs
@@ -1,7 +1,6 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use reqwest::StatusCode;
-use serde_json::json;
-
+use serde_json::{json, Value};
 
 #[tokio::test]
 async fn test_login_logout() -> Result<()> {
@@ -26,6 +25,52 @@ async fn test_login_logout() -> Result<()> {
 
     let status_response = client.delete("http://localhost:3000/login").send().await?;
     assert_eq!(status_response.status(), StatusCode::UNAUTHORIZED);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_create_table() -> Result<()> {
+    let client = reqwest::Client::builder().cookie_store(true).build()?;
+
+    let login_response = client
+        .post("http://localhost:3000/login")
+        .json(&json!({ "name": "Jane Doe" }))
+        .send()
+        .await?;
+    assert_eq!(login_response.status(), StatusCode::OK);
+
+    let table_status = client
+        .get("http://localhost:3000/table")
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        table_status
+            .as_array()
+            .context("expected array of tables")?
+            .len(),
+        0
+    );
+
+    let create_response = client
+        .post("http://localhost:3000/table")
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    let table_id = create_response["id"]
+        .as_str()
+        .context("table status should have id")?;
+
+    let table_status = client
+        .get(format!("http://localhost:3000/table/{}", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(table_status["id"], table_id);
 
     Ok(())
 }
