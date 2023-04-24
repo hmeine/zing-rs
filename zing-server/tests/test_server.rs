7bef122f47133b29cc3c866db9093f52b0e5e434
Hans Meine <hans_meine@gmx.net>
2023-04-24 22:12:19 +0200

    finish converting tests to rust

--- zing-server/tests/test_server.rs
+++ zing-server/tests/test_server.rs
@@ -40,14 +40,14 @@ async fn test_create_table() -> Result<()> {
         .await?;
     assert_eq!(login_response.status(), StatusCode::OK);
 
-    let table_status = client
+    let tables_status = client
         .get("http://localhost:3000/table")
         .send()
         .await?
         .json::<Value>()
         .await?;
     assert_eq!(
-        table_status
+        tables_status
             .as_array()
             .context("expected array of tables")?
             .len(),
@@ -64,6 +64,20 @@ async fn test_create_table() -> Result<()> {
         .as_str()
         .context("table status should have id")?;
 
+    let tables_status = client
+        .get("http://localhost:3000/table")
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        tables_status
+            .as_array()
+            .context("expected array of tables")?
+            .len(),
+        1
+    );
+
     let table_status = client
         .get(format!("http://localhost:3000/table/{}", table_id))
         .send()
@@ -71,6 +85,317 @@ async fn test_create_table() -> Result<()> {
         .json::<Value>()
         .await?;
     assert_eq!(table_status["id"], table_id);
+
+    let join_response = client
+        .post(format!("http://localhost:3000/table/{}", table_id))
+        .send()
+        .await?;
+    // must not be able to join table again
+    assert_eq!(join_response.status(), StatusCode::CONFLICT);
+
+    let tables_status = client
+        .get("http://localhost:3000/table")
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        tables_status
+            .as_array()
+            .context("expected array of tables")?
+            .len(),
+        1 // number of tables must not have changed
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_game_starting() -> Result<()> {
+    let client1 = reqwest::Client::builder().cookie_store(true).build()?;
+
+    client1
+        .post("http://localhost:3000/login")
+        .json(&json!({ "name": "Player 1" }))
+        .send()
+        .await?;
+
+    let create_response = client1
+        .post("http://localhost:3000/table")
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    let table_id = create_response["id"]
+        .as_str()
+        .context("table status should have id")?;
+
+    let start_response = client1
+        .post(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?;
+    // must not be able to start game with single player at table
+    assert_eq!(start_response.status(), StatusCode::CONFLICT);
+
+    let client2 = reqwest::Client::builder().cookie_store(true).build()?;
+
+    client2
+        .post("http://localhost:3000/login")
+        .json(&json!({ "name": "Player 2" }))
+        .send()
+        .await?;
+
+    let join_response = client2
+        .post(format!("http://localhost:3000/table/{}", table_id))
+        .send()
+        .await?;
+    assert_eq!(join_response.status(), StatusCode::OK);
+
+    let inactive_response = client2
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        inactive_response["active"]
+            .as_bool()
+            .context("game status should have active attribute")?,
+        false
+    );
+
+    let start_response = client2
+        .post(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?;
+    assert_eq!(start_response.status(), StatusCode::OK);
+
+    let active_response = client2
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        active_response["active"]
+            .as_bool()
+            .context("game status should have active attribute")?,
+        true
+    );
+
+    let active_response = client1
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        active_response["active"]
+            .as_bool()
+            .context("game status should have active attribute")?,
+        true
+    );
+
+    let start_response = client2
+        .post(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?;
+    assert_eq!(start_response.status(), StatusCode::CONFLICT);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_playing_cards() -> Result<()> {
+    let client2 = reqwest::Client::builder().cookie_store(true).build()?;
+
+    client2
+        .post("http://localhost:3000/login")
+        .json(&json!({ "name": "Player 2" }))
+        .send()
+        .await?;
+
+    let client1 = reqwest::Client::builder().cookie_store(true).build()?;
+
+    client1
+        .post("http://localhost:3000/login")
+        .json(&json!({ "name": "Player 1" }))
+        .send()
+        .await?;
+
+    let create_response = client1
+        .post("http://localhost:3000/table")
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    let table_id = create_response["id"]
+        .as_str()
+        .context("table status should have id")?;
+
+    client2
+        .post(format!("http://localhost:3000/table/{}", table_id))
+        .send()
+        .await?;
+
+    client1
+        .post(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?;
+
+    let play_response = client2
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 2 }))
+        .send()
+        .await?;
+    assert_eq!(play_response.status(), StatusCode::OK);
+
+    let play_response = client2
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 1 }))
+        .send()
+        .await?;
+    // playing when not one's turn:
+    assert_eq!(play_response.status(), StatusCode::CONFLICT);
+
+    let play_response = client1
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 3 }))
+        .send()
+        .await?;
+    assert_eq!(play_response.status(), StatusCode::OK);
+
+    let play_response = client1
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 0 }))
+        .send()
+        .await?;
+    // playing when not one's turn:
+    assert_eq!(play_response.status(), StatusCode::CONFLICT);
+
+    let play_response = client2
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 3 }))
+        .send()
+        .await?;
+    // too high card index
+    assert_eq!(play_response.status(), StatusCode::CONFLICT);
+
+    let ended_response = client2
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        ended_response["ended"]
+            .as_bool()
+            .context("game status should have ended attribute")?,
+        false
+    );
+
+    // echo "players have 48 cards in total; 2 have been played already, 2*23 to go..."
+    for i in 0..23 {
+        let play_response = client2
+            .post(format!(
+                "http://localhost:3000/table/{}/game/play",
+                table_id
+            ))
+            .json(&json!({ "card_index": 0 }))
+            .send()
+            .await?;
+        assert_eq!(play_response.status(), StatusCode::OK);
+
+        let play_response = client1
+            .post(format!(
+                "http://localhost:3000/table/{}/game/play",
+                table_id
+            ))
+            .json(&json!({ "card_index": 0 }))
+            .send()
+            .await?;
+        assert_eq!(play_response.status(), StatusCode::OK);
+    }
+
+    let play_response = client2
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 0 }))
+        .send()
+        .await?;
+    assert_eq!(play_response.status(), StatusCode::CONFLICT);
+
+    let play_response = client1
+        .post(format!(
+            "http://localhost:3000/table/{}/game/play",
+            table_id
+        ))
+        .json(&json!({ "card_index": 0 }))
+        .send()
+        .await?;
+    assert_eq!(play_response.status(), StatusCode::CONFLICT);
+
+    let ended_response = client2
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        ended_response["ended"]
+            .as_bool()
+            .context("game status should have ended attribute")?,
+        true
+    );
+
+    let active_response = client2
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        active_response["active"]
+            .as_bool()
+            .context("game status should have active attribute")?,
+        true
+    );
+
+    let finish_response = client1
+        .delete(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?;
+    assert_eq!(finish_response.status(), StatusCode::OK);
+
+    let inactive_response = client2
+        .get(format!("http://localhost:3000/table/{}/game", table_id))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    assert_eq!(
+        inactive_response["active"]
+            .as_bool()
+            .context("game status should have active attribute")?,
+        false
+    );
 
     Ok(())
 }
