858d938c2f6939b8e10c1ef3afbf9bdb2d8e0997
Hans Meine <hans_meine@gmx.net>
2023-09-30 18:17:59 +0200

    make bevy-inspector-egui branch a feature instead

--- zing-ui-lib/src/debug.rs
+++ zing-ui-lib/src/debug.rs
@@ -1,11 +1,14 @@
 use bevy::prelude::*;
+#[cfg(feature = "bevy-inspector-egui")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
+#[cfg(feature = "bevy-inspector-egui")]
 use crate::{card_sprite::CardSprite, zing_layout::CardStack};
 
 pub struct DebugPlugin;
 
 impl Plugin for DebugPlugin {
+    #[cfg(feature = "bevy-inspector-egui")]
     fn build(&self, app: &mut App) {
         if cfg!(debug_assertions) {
             app.add_plugins(WorldInspectorPlugin::new());
@@ -13,4 +16,7 @@ impl Plugin for DebugPlugin {
             app.register_type::<CardSprite>();
         }
     }
+
+    #[cfg(not(feature = "bevy-inspector-egui"))]
+    fn build(&self, _app: &mut App) {}
 }
